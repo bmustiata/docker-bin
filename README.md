93ee027c1208a7dc0438cfb53e4e1a9169582188
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-11-12 10:03:37 +0100

    Added cadvisor and gif-create.

---
+++ README.md
@@ -0,0 +1,1 @@
+A bunch of shell scripts just to launch docker appliances.
---
+++ cadvisor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#
+# Google's cAdvisor
+#
+
+docker run \
+    --volume=/:/rootfs:ro\
+    --volume=/var/run:/var/run:rw\
+    --volume=/sys:/sys:ro\
+    --volume=/var/lib/docker/:/var/lib/docker:ro\
+    --publish=8080:8080\
+    --detach=true\
+    --name=cadvisor\
+    --rm\
+    google/cadvisor:latest
+

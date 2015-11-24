60b0895e0262a35d3a1070a45ad7914b23ccbea6
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-11-24 16:38:33 +0100

    Use the right group as well.

--- mysqld.sh
+++ mysqld.sh
@@ -4,6 +4,7 @@ docker run \
     --name mysqld\
     -e MYSQL_ROOT_PASSWORD='asd' \
     -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
     -v `pwd`:/var/lib/mysql/ \
     -v /var/run/mysqld/:/var/run/mysqld/ \
     -p 3306:3306 \

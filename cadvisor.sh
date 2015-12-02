3fbe3f8acc4cb7217a44e4c84e76505494458031
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-12-02 17:05:15 +0100

    Changed cadvisor to listen on 8090.

--- cadvisor.sh
+++ cadvisor.sh
@@ -9,9 +9,8 @@ docker run \
     --volume=/var/run:/var/run:rw\
     --volume=/sys:/sys:ro\
     --volume=/var/lib/docker/:/var/lib/docker:ro\
-    --publish=8080:8080\
+    --publish=8090:8080\
     --detach=true\
     --name=cadvisor\
-    --rm\
     google/cadvisor:latest
 

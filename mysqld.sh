c278e3c59a1b1b0ce689aa1afbd8117f150a47a7
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-05-20 12:18:29 +0200

    Mysqld fix. Update the kubernetes.

--- mysqld.sh
+++ mysqld.sh
@@ -8,11 +8,12 @@ fi   # else [[ "$CONTAINER_NAME" == "" ]]
 
 docker run \
     --name $CONTAINER_NAME\
-    -e MYSQL_ROOT_PASSWORD='asd' \
+    -e MYSQL_ROOT_PASSWORD='root' \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
     -v `pwd`:/var/lib/mysql/ \
     -v /var/run/mysqld/:/var/run/mysqld/ \
     -p 3306:3306 \
+    -d \
     mysql
 

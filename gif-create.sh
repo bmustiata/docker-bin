64cf333e64940636168717f614ec500931d9306d
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-07-09 23:28:45 +0200

    Added scaling.

--- gif-create.sh
+++ gif-create.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ "$3" == "" ]]; then
+    SCALING_FACTOR="1.0"
+else # not [[ "$3" == "" ]]
+    SCALING_FACTOR="$3"
+fi   # else [[ "$3" == "" ]]
+
+echo "Scaling factor is: $3"
 
 docker run \
     --rm \
     -v $1:/tmp/in/test.avi \
     -v $(dirname $2):/tmp/write \
     -e OUTPUT_FILE_NAME=$(basename $2) \
+    -e SCALING_FACTOR=$SCALING_FACTOR \
     bmst/gif-create
 

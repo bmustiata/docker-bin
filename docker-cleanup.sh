0ff107415be7f2e0d2c4f08fb9e1e2ecd11314a0
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-10-21 17:38:18 +0200

    Allow cleaning also only created containers that were not started.

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 docker rm -v $(docker ps -a | grep Exited | tr -s " " | cut -f1 -d\ )
+
+if [[ "$1" == "-a" ]]; then
+    docker rm -v $(docker ps -a | grep Created | tr -s " " | cut -f1 -d\ )
+fi # [[ "$1" == "-a" ]]
+
 docker rmi $(docker images | grep '<none>' | tr -s " " | cut -f3 -d\ )
